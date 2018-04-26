01ca08b6676222cb1aeb1c903593388ad2c1e507
Benjamin Liu <ben@mightysignal.com>
2018-04-26 12:20:21 -0700

    Add correct download url

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Benjamin Liu',
   author_email = 'ben@mightysignal.com',
   url = 'https://github.com/benliu01/route53_weighted_records',
-  download_url = 'https://github.com/benliu01/route53_weighted_records/archive/0.1.tar.gz',
+  download_url = 'https://github.com/benliu01/route53_weighted_records/archive/0.3.tar.gz',
   keywords = ['dns', 'route53', 'aws', 'records'],
   install_requires=[
     'boto3>=1.4.4'
