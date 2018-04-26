4e2d413de50ec556d03c29734f2c7c401266fe77
Benjamin Liu <the.ben.liu@gmail.com>
2018-04-25 23:42:26 -0700

    Adding package files

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+  name = 'route53weightedrecords',
+  packages = ['route53weightedrecords'],
+  version = '0.1',
+  description = 'Python package to manage over 100 weighted records in AWS Route53.',
+  author = 'Benjamin Liu',
+  author_email = 'ben@mightysignal.com',
+  url = 'https://github.com/benliu01/route53_weighted_records',
+  download_url = 'https://github.com/benliu01/route53_weighted_records/archive/0.1.tar.gz',
+  keywords = ['dns', 'route53', 'aws', 'records'],
+  install_requires=[
+    'boto3>=1.4.4'
+  ],
+  classifiers = []
+)
